8c2a62a0d1457532ca0dce832bfa762ea2e46498
Legoben <legoben1998@gmail.com>
2015-02-14 14:46:11 -0600

    Added android and geolocation

--- server/main.py
+++ server/main.py
@@ -1,3 +1,17 @@
-import tornado
+from tornado import ioloop, web, websocket
 import pymongo
+
+
+class MainHandler(web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    ioloop.IOLoop.instance().start()
+
 
