fbc486c260fbe43673eaf59185bbac33279cd6a8
Tristan Seifert <tristan@squee.co>
2015-02-14 17:27:32 -0600

    Added loader and lots of fun animations

--- www/js/index.js
+++ www/js/index.js
@@ -2,17 +2,31 @@
  * App config
  */
 window.config = {
-    api: "http://ping.ngrok.com/"
+    api: "http://ping.ngrok.com/",
+
+    fadeLength: 333
 };
 
 /**
  * Initialiser
  */
 $(document).ready(function() {
-    // bind a bunch of stuffs pls.
+    // showing of the "join existing" dialogue
     $(".home .btn-join").click(function(e) {
         e.preventDefault();
 
-        alert("plsss m8");
+        $(".home .intro").slideToggle(window.config.fadeLength);
+        $(".home .join").slideToggle(window.config.fadeLength);
     });
+
+    // exit the "join existing" dialogue to the home screen
+    $(".home .btn-cancel-join-room").click(function(e) {
+        e.preventDefault();
+
+        $(".home .intro").slideToggle(window.config.fadeLength);
+        $(".home .join").slideToggle(window.config.fadeLength);
+    });
+
+    // hide loading indicator
+    Pace.stop();
 });
