855e589b3d6f54b5db9b4b1f6c8be75d5394805e
Tristan Seifert <tristan@squee.co>
2015-02-14 20:46:59 -0600

    Added some debug info

--- www/js/game.js
+++ www/js/game.js
@@ -49,19 +49,33 @@ function locationUpdated(position) {
     console.log(position);
 
     ws.send(JSON.stringify({
-        "event": "updateloc",
+        event: "updateloc",
         data: {
-            "lon": position.coords.longitude,
-            "lat": position.coords.latitude,
-            "alt": position.coords.altitude,
-            "accuracy": position.coords.accuracy,
-            "pid": myid
+            lon: position.coords.longitude,
+            lat: position.coords.latitude,
+            alt: position.coords.altitude,
+            accuracy: position.coords.accuracy,
+            pid: myid,
+
+            device: {
+                uuid: device.uuid,
+
+                name: device.model,
+                platform: device.platform,
+                version: device.version
+            }
         }, 
         gid: gid
     }));
 
+    // Update the debug information
+    $(".game #debug-pos").html("(" + position.coords.latitude + ", " + position.coords.longitude + ")");
+    $(".game #debug-altitude").html(position.coords.altitude + "m");
+    $(".game #debug-accuracy").html(position.coords.accuracy + "m");
+
     // hide the alert, if it's visible
     $(".game .alert-location-invalid").fadeOut(window.config.fadeLength);
+    $(".game .alert-location-error").fadeOut(window.config.fadeLength);
 }
 
 // An error occurred while getting the location.
@@ -75,5 +89,8 @@ function locationError(error) {
     } else if(error.code == PositionError.POSITION_UNAVAILABLE) {
         // Show an UI indicator
         $(".game .alert-location-invalid").fadeIn(window.config.fadeLength);
+    } else {
+        $(".game .alert-location-error p:first-child").html(error.message);
+        $(".game .alert-location-error").fadeIn(window.config.fadeLength);
     }
 }
